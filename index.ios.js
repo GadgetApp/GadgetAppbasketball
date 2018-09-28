add1cf41487e416e597f6b7a866753202009f300
kandarp007 <davekandarpc@gmail.com>
2018-09-28 17:01:32 +0530

    Initial commit for kandarp to show source code demo to customer

---
+++ index.ios.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+
+import Basketball from './Basketball';
+
+AppRegistry.registerComponent('Basketball', () => Basketball);
+
